a013bae9f66551d8d4176e8adc2b6c2d98adb676
owais-dotcom <owaissayed913@gmail.com>
2026-03-09 23:03:20 +0530

    Update site URL and configure Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
-  site: 'https://bodynation.in',
+  site: 'https://bodynation-website.vercel.app/',
+  output: 'static',
+  adapter: vercel(),
   integrations: [
     tailwind({ applyBaseStyles: false }),
-    sitemap(),
     react(),
   ],
   image: {
